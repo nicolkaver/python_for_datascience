a8007bb414ac1669e168a5a3a70e31d7fd03268f
Nikola BORATKOVA <nboratko@made-f0Dr6s11.clusters.42paris.fr>
2023-12-01 15:40:08 +0100

    ADD EX02:
    files to add: find_ft_type

---
+++ ex02/find_ft_type.py
@@ -0,0 +1,13 @@
+def all_thing_is_obj(object: any):
+    if type(object) == list:
+        print(f"List : {type(object)}")
+    elif type(object) == set:
+        print(f"Set : {type(object)}")
+    elif type(object) == dict:
+        print(f"Dict : {type(object)}")
+    elif type(object) == str:
+        print(f"{object} is in the kitchen : {type(object)}")
+    else:
+        print("Type not found")
+
+    return 42
