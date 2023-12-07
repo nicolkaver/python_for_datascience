846b3f808e9186c33868778c6a7e3c5cb5d5335e
Nikola <nikol.boratkova@gmail.com>
2023-12-07 15:47:33 +0000

    ADD EX01 OF DAY01:
    submitted file: array2D.py

--- day01/ex01/array2D.py
+++ day01/ex01/array2D.py
@@ -1,3 +1,19 @@
 import numpy as np
+import sys
 
 def slice_me(family: list, start: int, end: int) -> list:
+    np_array = np.array(family)
+    try:
+        assert isinstance(family, list), "Family argument must be a list"
+        same_length = (np.diff(np_array.shape[:-1]) == 0).all()
+        assert same_length == True, "All rows do not have the same length"
+    except AssertionError as e:
+        print(f"{type(e).__name__} : {e}")
+        sys.exit(1)
+
+    print(f"My shape is : {np_array.shape}")
+
+    truncated_array = np_array[start : end]
+    print("My new shape is : ")
+
+    return truncated_array.tolist()
