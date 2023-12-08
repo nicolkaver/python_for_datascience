223652b9346543db647ca1fbff7644d892cd7b98
Nikola <nikol.boratkova@gmail.com>
2023-12-08 12:33:57 +0000

    UPDATE FINISHED EX03 OF DAY01:
    submitted file: zoom.py

--- day01/ex03/zoom.py
+++ day01/ex03/zoom.py
@@ -1,6 +1,7 @@
 from load_image import ft_load
 import numpy as np
 from PIL import Image, ImageDraw, ImageFont
+import cv2
 
 def ft_zoom(path: str) -> Image.Image:
     img = Image.open(path)
@@ -16,18 +17,37 @@ def ft_zoom(path: str) -> Image.Image:
 
     bw_cropped_img = bw_cropped_img.crop((left, top, right, bottom))
 
+    bw_cropped_cv = np.array(bw_cropped_img)
+    contours, _ = cv2.findContours(bw_cropped_cv, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cv2.drawContours(bw_cropped_cv, contours, -1, (0, 0, 0), 2)  # Contours color: white (255)
+
+    # Convert OpenCV image back to PIL image for display
+    contour_pil_image = Image.fromarray(bw_cropped_cv)
+
     tmp_array = np.array(bw_cropped_img)
     print(f"New shape after slicing: {tmp_array.shape}")
     
     # add scale labels
-    draw = ImageDraw.Draw(bw_cropped_img)
-    width, height = bw_cropped_img.size
-    font = ImageFont.truetype("arial.tff", 16)
+    width, height = contour_pil_image.size
 
-    for x in range(0, width, 50):
-        draw.text((x, height - 20), f"{x}px", fill="white", font=font)
+    new_width = width + 200
+    new_height = height + 200
+    extended_img = Image.new("RGB", (new_width, new_height), color="white")
+    extended_img.paste(contour_pil_image, (100, 100)) # Paste the original image at (100, 100)
 
-    return bw_cropped_img
+    # Draw is for creating the drawing context
+    draw = ImageDraw.Draw(extended_img)
+    # font_path = "./arial.ttf"
+    # font = ImageFont.truetype(font_path, 16)
+    font = ImageFont.load_default()
+
+    for x in range(100, new_width - 100, 50):
+        draw.text((x, new_height - 75), str(x - 100), fill="black", font=font)
+
+    for y in range(100, new_height - 100, 50):
+        draw.text((60, y), str(y -100), fill="black", font=font)
+
+    return extended_img
 
 def main():
     # print(ft_load("animal.jpeg"))
