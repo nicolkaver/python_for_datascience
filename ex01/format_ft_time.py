dacb94e556a87efbbf2e8105ddba91c623bd1af1
Nikola <nikol.boratkova@gmail.com>
2023-12-13 10:16:59 +0000

    REMOVED ALL EX FILES:
    They were duplicates of ex files in day00

--- ex01/format_ft_time.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-
-now = datetime.now()
-ts = datetime.timestamp(now)
-formated_ts = ("{:,}".format(ts))
-scientific_format = "{:.2e}".format(ts) 
-print ("Seconds since January, 1, 1970:", formated_ts, "or", scientific_format, "in scientific notation")
-print(now.strftime("%b %d %Y"))
--- ex06/ft_filter.py
+++
@@ -1,2 +0,0 @@
-def ft_filter(func, iterable):
-    return (item for item in iterable if func(item))
